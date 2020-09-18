6de117688841cf278b5ba64d644389b1ce9fcb52
raybednara <ray.bednara@att.net>
2020-09-17 20:51:38 -0500

    add fields handling to toggles

--- client/lib/form/toggles.js
+++ client/lib/form/toggles.js
@@ -2,6 +2,13 @@ const form = document.querySelector("form");
 
 const fields = form.querySelectorAll("div:not(:last-of-type)");
 
+//TODO: Write a filter that will return out anything that does not have a data-status attribute (hasAttribute("data-status"))
+
+const regularFields = [...fields].filter(
+  (field) => !field.hasAttributes("data-status")
+);
+
 export default () => {
   console.log(fields);
+  console.log(regularFields);
 };
